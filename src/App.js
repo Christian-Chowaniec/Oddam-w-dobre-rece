81c2df84010288ca24aecc3ee70e9e9de0ed3413
Christian-Chowaniec <christian.chowaniec03@gmail.com>
2021-10-27 02:16:45 +0200

    innit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Home from "./components/home";
+import Navigation from "./components/navigation"
+import UserButtons from "./components/user/user";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          siema mordouuu
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <UserButtons/>
+            <Navigation/>
+            <Home/>
+            <h2>lol</h2>
+        </>
+    );
 }
 
+
 export default App;
+
+//
+// <div>
+//
+//     <header>
+//
+//         <p className="bla__bla-bla">bla blad blas</p>
+//         <a>siema mordouuu</a>
+//
+//     </header>
+// </div>
---
+++ src/components/navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Start</li>
+                <li>O co chodzi?</li>
+                <li>O nas</li>
+                <li>Fundacja i organizacje</li>
+                <li>Kontakt</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
