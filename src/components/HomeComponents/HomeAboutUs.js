318956e07566163b650787f6fae929cdd36fd03a
Christian-Chowaniec <christian.chowaniec03@gmail.com>
2021-11-04 04:27:30 +0100

    add HomeAboutUs

--- src/components/HomeComponents/HomeAboutUs.js
+++ src/components/HomeComponents/HomeAboutUs.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const HomeAboutUs = () => {
-    return (
-        <div>
-            <h1>HomeAboutUs</h1>
+	return (
+		<section id="about_us">
+			<div className="HomeAboutUs__box">
+				<div className="HomeAboutUs__left_side">
+					<div className="HomeAboutUs__items">
+						<h2 className="HomeAboutUs__title">O nas</h2>
+						<div className="decoration" />
+						<p className="HomeAboutUs__description">
+							Nori grape silver beet broccoli kombu beet greens fava bean potato
+							quandong celery. Bunya nuts black-eyed pea prairie turnip leek
+							lentil turnip greens parsnip.
+						</p>
+						<div className="HomeAboutUs__signature_box">
+							<div className="HomeAboutUs__signature" />
+						</div>
+					</div>
+				</div>
 
-        </div>
-    );
+				<div className="HomeAboutUs__right_side">
+					<div className="HomeAboutUs__img" />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default HomeAboutUs;
