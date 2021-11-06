4839398b2199cd27e1411114ed6dfa63324dc4ad
Christian-Chowaniec <christian.chowaniec03@gmail.com>
2021-11-06 13:15:25 +0100

    fixed Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,52 +6,35 @@ const Navigation = () => {
 	return (
 		<section>
 			<nav className="Navigation__menu">
-        
 				<ul className="Navigation__menu_ul">
 					<li>
-						<NavLink exact to="/">
-							<a className="Navigation__menu_li">Start</a>
+						<NavLink exact to="/" className="Nav_item">
+							<div className="Navigation__menu_li">Start</div>
 						</NavLink>
 					</li>
 
 					<li>
-						<Link to="simple_steps"
-						smooth={true}
-						duration={500}
-						offset={50}>
-							<a className="Navigation__menu_li">O co chodzi?</a>
+						<Link to="simple_steps" smooth={true} duration={500} offset={50}>
+							<div className="Navigation__menu_li">O co chodzi?</div>
 						</Link>
 					</li>
 
 					<li>
-						<Link to="about_us"
-						smooth={true}
-						duration={500}
-						offset={50}>
-							<a className="Navigation__menu_li">O nas</a>
-              
-						</Link>
-					</li>
-        
-
-					<li>
-						<Link to="who_we_help"
-						smooth={true}
-						duration={500}
-						offset={50}>
-							<a className="Navigation__menu_li">Fundacja i organizacje</a>
+						<Link to="about_us" smooth={true} duration={500} offset={50}>
+							<div className="Navigation__menu_li">O nas</div>
 						</Link>
 					</li>
 
 					<li>
-					
-						<Link to="contact_us"
-						smooth={true}
-						duration={500}
-						offset={50}>
-							<a className="Navigation__menu_li">Kontakt</a>
+						<Link to="who_we_help" smooth={true} duration={500} offset={50}>
+							<div className="Navigation__menu_li">Fundacja i organizacje</div>
 						</Link>
-						
+					</li>
+
+					<li>
+						<Link to="contact_us" smooth={true} duration={500} offset={50}>
+							<div className="Navigation__menu_li">Kontakt</div>
+						</Link>
 					</li>
 				</ul>
 			</nav>
@@ -60,4 +43,3 @@ const Navigation = () => {
 };
 
 export default Navigation;
-
